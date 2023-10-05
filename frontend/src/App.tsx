5ce5af0d469f4392f3aa2b4ffb3f24f579c3cd35
rickyxyz <56119701+rickyxyz@users.noreply.github.com>
2023-10-06 01:46:58 +0700

    feat: add search bar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,38 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import axios from "axios";
 import './App.css'
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
-  axios
-  .get("/api/university/1")
-  .then((res) => console.log(res))
+  const [query, setQuery] = useState('');
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setQuery(event.target.value)
+  }
+
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    axios
+    .get("/api/search/", {params: {q: query}})
+    .then((res) => console.log(res))
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <form onSubmit={handleSubmit} method="POST">
+      <input type="text" name="query" id="query" onChange={handleInputChange} />
+      <button type="submit">search</button>
+    </form>
   )
 }
 
